78b7a3933dc62a1e940b2cde3d8d86e0bb6ce409
gavin <kwongavin@icloud.com>
2023-03-16 10:06:26 -0700

    Initial Commit

---
+++ KFormatScreening/KFormatScreeningApp.swift
@@ -0,0 +1,17 @@
+//
+//  KFormatScreeningApp.swift
+//  KFormatScreening
+//
+//  Created by Gavin Kwon on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct KFormatScreeningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
