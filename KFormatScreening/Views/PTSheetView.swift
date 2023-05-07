c813391f9a912a60e35aede00c362c57e438bdf0
Gavin Kwon <kwongavin@gmail.com>
2023-05-06 22:26:56 -0700

    - Implemented .sheet as ShowPreviews for each companies

---
+++ KFormatScreening/Views/PTSheetView.swift
@@ -0,0 +1,168 @@
+//
+//  PTSheetView.swift
+//  KFormatScreening
+//
+//  Created by Gavin Kwon on 5/6/23.
+//
+
+import SwiftUI
+import AVKit
+
+
+struct PTSheetView: View {
+    
+    @State var enteredShowType: String
+    @State var enteredShowCompany: String
+    @State var enteredShowPerson: String
+    @State var enteredShowPosition: String
+    @State var enteredShowEmail: String
+    @State var enteredShowPhone: String
+    @State var enteredShowName: String
+    @State var enteredShowImage: String
+    @State var enteredShowLogo: String
+    @State var enteredShowColor: String
+    @State var enteredShowYear: String
+    @State var enteredShowGenre: String
+    @State var enteredShowDescription: String
+    @State var enteredShowTime: String
+    @State var enteredShowEpisodes: String
+    @State var enteredShowUrl: String
+    
+    @State private var player: AVPlayer?
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            
+            HStack(spacing: 15) {
+//                Button(action: {
+//                    presentationMode.wrappedValue.dismiss()
+//                }, label: {
+//                    Image(systemName: "arrowshape.backward.fill")
+//                        .padding(.leading, -15)
+//                        .foregroundColor(Color(enteredShowColor))
+//                })
+                Text(enteredShowName)
+                    .font(Font.custom("Avenir Black", size: 24))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Button(action: {
+                    presentationMode.wrappedValue.dismiss()
+                }, label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(Color("color_event"))
+                })
+//                Image(enteredShowLogo)
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(height: 30)
+//                    .clipShape(Circle())
+//                    .overlay {
+//                        Circle().stroke(Color(enteredShowColor), lineWidth: 3)
+//                    }
+//                    .opacity(0.5)
+//                    .padding([.trailing, .bottom], 3)
+            } .padding(.horizontal)
+            
+            let url = URL(string: enteredShowUrl)
+            if url != nil {
+                VideoPlayer(player: player)
+                    .aspectRatio(16/9, contentMode: .fit)
+                    .cornerRadius(10)
+                    .onAppear(perform: {
+                        self.player = AVPlayer(url: url!)
+                        self.player?.play()
+                    })
+                    .onDisappear {
+                        self.player?.pause()
+                        self.player = nil
+                    }
+            }
+            
+            ScrollView(showsIndicators: false) {
+                
+                DashedDivider()
+                    .padding(.vertical, 10)
+                
+                ZStack(alignment: .leading) {
+                    RectanglePreset()
+                        .foregroundColor(Color(enteredShowColor))
+                        .padding(.top, 5)
+                        .opacity(0.2)
+                        .frame(maxWidth: .infinity)
+                        .aspectRatio(16/4.5,contentMode: .fit)
+                    
+                    VStack(alignment: .leading, spacing: 5) {
+                        HStack {
+                            Text(enteredShowYear + "  | ")
+                                .font(Font.custom("Avenir Roman", size: 18))
+                            Text(enteredShowGenre)
+                                .font(Font.custom("Avenir Roman", size: 18))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                        
+                        HStack {
+                            Text(enteredShowEpisodes + "  | ")
+                                .font(Font.custom("Avenir Roman", size: 18))
+                            Text(enteredShowTime)
+                                .font(Font.custom("Avenir Roman", size: 18))
+                        }
+                    } .padding()
+                }
+                
+                DashedDivider()
+                    .padding(.vertical, 12)
+                
+                VStack(alignment: .leading) {
+                    Text(enteredShowDescription)
+                        .font(Font.custom("Avenir", size: 19))
+                        .padding(.horizontal)
+                }
+                
+                DashedDivider()
+                    .padding(.vertical, 10)
+                
+                ZStack(alignment: .leading) {
+                    RectanglePreset()
+                        .foregroundColor(Color(enteredShowColor))
+                        .padding(.top, 5)
+                        .opacity(0.2)
+                        .frame(maxWidth: .infinity)
+                        .aspectRatio(16/10,contentMode: .fit)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        
+                        Text(enteredShowCompany)
+                            .font(Font.custom("Avenir Black", size: 22))
+                        Divider()
+                            .padding(.vertical, 10)
+                            Text(enteredShowPerson)
+                                .font(Font.custom("Avenir Heavy", size: 20))
+                            Text(enteredShowPosition)
+                                .font(Font.custom("Avenir", size: 18))
+                        
+                        HStack {
+                            Link(enteredShowPhone + "  | ", destination: URL(string: "tel:\(enteredShowPhone)")!)
+                                .accentColor(colorScheme == .dark ? .white: .black)
+                                .font(Font.custom("Avenir Roman", size: 15))
+                            Link(enteredShowEmail, destination: URL(string: "mailto:\(enteredShowEmail)")!)
+                                .accentColor(colorScheme == .dark ? .white: Color("color_event"))
+                                .padding(.top, 1)
+                                .font(Font.custom("Avenir Roman", size: 15))
+                        }
+                    } .padding()
+                }
+            }
+            
+        }
+        .navigationBarBackButtonHidden(true)
+        .padding()
+    }
+}
+
+struct PTSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        PTSheetView(enteredShowType: "Non-Scripted", enteredShowCompany: "MBC America", enteredShowPerson: "Hyewon Chung", enteredShowPosition: "Director", enteredShowEmail: "chung@kbsmedia.co.kr", enteredShowPhone: "1-323-935-5001", enteredShowName: "Euphoria 2025", enteredShowImage: "show_mbc1", enteredShowLogo: "logo_kbs", enteredShowColor: "color_kbs", enteredShowYear: "2023", enteredShowGenre: "Comedy, Thriller", enteredShowDescription: "Shaun Murphy, a young autistic surgeon who has savant syndrome, relocates from a quiet country life to join the surgical unit at the prestigious San Jose St. Bonaventure Hospital -- a move strongly supported by his mentor, Dr. Aaron Glassman. Having survived a troubled childhood, Shaun is alone in the world and unable to personally connect with those around him, but he finds his niche using his extraordinary medical skill and intuition to save lives and challenge the skepticism of his colleagues.", enteredShowTime: "70 Minutes", enteredShowEpisodes: "17 Episodes", enteredShowUrl: "https://github.com/kwongavin/kformatscreening/blob/main/muscle1.mp4?raw=true")
+    }
+}
--- KFormatScreening/Views/PTVideoView.swift
+++ KFormatScreening/Views/PTVideoView.swift
@@ -18,7 +18,24 @@ struct PTVideoView: View {
     
     @State var selectedCompanyIndex: Int
     @State private var player: AVPlayer?
-//    @State private var isFullScreen = false
+    @State var isSheetShowing = false
+    
+    @State var enteredShowType = ""
+    @State var enteredShowCompany = ""
+    @State var enteredShowPerson = ""
+    @State var enteredShowPosition = ""
+    @State var enteredShowEmail = ""
+    @State var enteredShowPhone = ""
+    @State var enteredShowName = ""
+    @State var enteredShowImage = ""
+    @State var enteredShowLogo = ""
+    @State var enteredShowColor = ""
+    @State var enteredShowYear = ""
+    @State var enteredShowGenre = ""
+    @State var enteredShowDescription = ""
+    @State var enteredShowTime = ""
+    @State var enteredShowEpisodes = ""
+    @State var enteredShowUrl = ""
     
     var body: some View {
         
@@ -46,65 +63,107 @@ struct PTVideoView: View {
                     .opacity(0.6)
                     .padding(.trailing, 3)
             } .padding(.horizontal)
+                .onAppear {
+                    loadCompanyShows()
+                }
             
-//            let url = URL(string: company.companies[selectedCompanyIndex].companyPTurl)
-//            if url != nil {
-//                VideoPlayer(player: player) // AVPlayer(url: url!))
-//                    .aspectRatio(16/9, contentMode: .fit)
-//                    .cornerRadius(10)
-//                    .onAppear(perform: {
-//                        self.player = AVPlayer(url: url!)
-//                        self.player?.play()
-//                    })
-//                    .onDisappear {
-//                        self.player?.pause()
-//                        self.player = nil
-////                        AVPlayer().replaceCurrentItem(with: nil)
-//                    }
-//            }
-            
-            ScrollView(showsIndicators: false) {
-                
-//                DashedDivider()
-//                    .padding(.vertical, 10)
-                
-                ZStack(alignment: .leading) {
-                    RectanglePreset()
-                        .foregroundColor(Color(company.companies[selectedCompanyIndex].companyColor))
-                        .padding(.top, 5)
-                        .opacity(0.3)
-                        .frame(maxWidth: .infinity)
-                        .aspectRatio(16/8,contentMode: .fit)
+            NavigationView {
+                ScrollView(showsIndicators: false) {
                     
-                    VStack(alignment: .leading, spacing: 10) {
+                    ZStack(alignment: .leading) {
+                        RectanglePreset()
+                            .foregroundColor(Color(company.companies[selectedCompanyIndex].companyColor))
+                            .padding(.top, 5)
+                            .opacity(0.3)
+                            .frame(maxWidth: .infinity)
+                            .aspectRatio(16/8,contentMode: .fit)
+                        
+                        VStack(alignment: .leading, spacing: 10) {
                             Text(company.companies[selectedCompanyIndex].companyPerson)
                                 .font(Font.custom("Avenir Heavy", size: 20))
                             Text(company.companies[selectedCompanyIndex].companyPosition)
                                 .font(Font.custom("Avenir", size: 18))
+                            
+                            HStack {
+                                Link(company.companies[selectedCompanyIndex].companyPhone + "  | ", destination: URL(string: "tel:\(company.companies[selectedCompanyIndex].companyPhone)")!)
+                                    .accentColor(colorScheme == .dark ? .white: .black)
+                                    .font(Font.custom("Avenir Roman", size: 15))
+                                Link(company.companies[selectedCompanyIndex].companyEmail, destination: URL(string: "mailto:\(company.companies[selectedCompanyIndex].companyEmail)")!)
+                                    .accentColor(colorScheme == .dark ? .white: Color("color_event"))
+                                    .padding(.top, 1)
+                                    .font(Font.custom("Avenir Roman", size: 15))
+                            }
+                            .padding(.top, 10)
+                            Link(company.companies[selectedCompanyIndex].companyUrl, destination: URL(string: "\(company.companies[selectedCompanyIndex].companyUrl)")!)
+                                .accentColor(.black)
+                                .font(Font.custom("Avenir Roman", size: 15))
+                        } .padding()
+                    }
+                    
+                    DashedDivider()
+                        .padding(.vertical, 12)
+                    
+                    VStack(alignment: .leading) {
+                        Text(company.companies[selectedCompanyIndex].companyDescription)
+                            .font(Font.custom("Avenir", size: 19))
+                            .padding(.horizontal)
+                    }
+                    
+                    DashedDivider()
+                        .padding(.vertical, 12)
+                        .padding(.bottom, 10)
+                    
+                    ForEach(companyShows.filter { $0.showCompany == company.companies[selectedCompanyIndex].companyName }) { show in
                         
-                        HStack {
-                            Link(company.companies[selectedCompanyIndex].companyPhone + "  | ", destination: URL(string: "tel:\(company.companies[selectedCompanyIndex].companyPhone)")!)
-                                .accentColor(colorScheme == .dark ? .white: .black)
-                                .font(Font.custom("Avenir Roman", size: 15))
-                            Link(company.companies[selectedCompanyIndex].companyEmail, destination: URL(string: "mailto:\(company.companies[selectedCompanyIndex].companyEmail)")!)
-                                .accentColor(colorScheme == .dark ? .white: Color("color_event"))
-                                .padding(.top, 1)
-                                .font(Font.custom("Avenir Roman", size: 15))
+                        Button(action: {
+                            enteredShowType = show.showType
+                            enteredShowCompany = show.showCompany
+                            enteredShowPerson = show.showPerson
+                            enteredShowPosition = show.showPosition
+                            enteredShowEmail = show.showEmail
+                            enteredShowPhone = show.showPhone
+                            enteredShowName = show.showName
+                            enteredShowImage = show.showImage
+                            enteredShowLogo = show.showLogo
+                            enteredShowColor = show.showColor
+                            enteredShowYear = show.showYear
+                            enteredShowGenre = show.showGenre
+                            enteredShowDescription = show.showDescription
+                            enteredShowTime = show.showTime
+                            enteredShowEpisodes = show.showEpisodes
+                            enteredShowUrl = show.showUrl
+                            
+                            isSheetShowing = true
+                        }, label: {
+                            HStack {
+                                Image(show.showImage)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100)
+                                    .cornerRadius(20)
+                                    .padding(.trailing, 20)
+                                VStack(alignment: .leading) {
+                                    HStack {
+                                        Text(show.showName)
+                                            .font(Font.custom("Avenir Black", size: 20))
+                                            .padding(.bottom, 1)
+                                        Spacer()
+                                    }
+                                    Divider()
+                                    Text(show.showYear + "  |  " + show.showType)
+                                        .font(Font.custom("Avenir", size: 16))
+                                        .padding(.top, 15)
+                                    Text(show.showTime + "  |  " + show.showGenre)
+                                        .font(Font.custom("Avenir", size: 16))
+                                }
+                            }
+                            .accentColor(colorScheme == .dark ? .white: .black)
+                            .padding(.bottom, 25)
+                        })
+                        .sheet(isPresented: $isSheetShowing) {
+                            PTSheetView(enteredShowType: enteredShowType, enteredShowCompany: enteredShowCompany, enteredShowPerson: enteredShowPerson, enteredShowPosition: enteredShowPosition, enteredShowEmail: enteredShowEmail, enteredShowPhone: enteredShowPhone, enteredShowName: enteredShowName, enteredShowImage: enteredShowImage, enteredShowLogo: enteredShowLogo, enteredShowColor: enteredShowColor, enteredShowYear: enteredShowYear, enteredShowGenre: enteredShowGenre, enteredShowDescription: enteredShowDescription, enteredShowTime: enteredShowTime, enteredShowEpisodes: enteredShowEpisodes, enteredShowUrl: enteredShowUrl)
                         }
-                        .padding(.top, 10)
-                        Link(company.companies[selectedCompanyIndex].companyUrl, destination: URL(string: "\(company.companies[selectedCompanyIndex].companyUrl)")!)
-                            .accentColor(.black)
-                            .font(Font.custom("Avenir Roman", size: 15))
-                    } .padding()
-                }
-                
-                DashedDivider()
-                    .padding(.vertical, 12)
-                
-                VStack(alignment: .leading) {
-                    Text(company.companies[selectedCompanyIndex].companyDescription)
-                        .font(Font.custom("Avenir", size: 19))
-                        .padding(.horizontal)
+                    }
                 }
             }
             
@@ -113,11 +172,17 @@ struct PTVideoView: View {
         .navigationBarBackButtonHidden(true)
         .padding()
     }
+    
+    func loadCompanyShows() {
+        let allShows = company.companies.flatMap { $0.companyShow }
+        companyShows = allShows
+    }
+    
 }
 
 struct PTVideoView_Previews: PreviewProvider {
     static var previews: some View {
-        PTVideoView(selectedCompanyIndex: 0)
+        PTVideoView(selectedCompanyIndex: 0, enteredShowType: "Non-Scripted", enteredShowCompany: "MBC America", enteredShowPerson: "Hyewon Chung", enteredShowPosition: "Director", enteredShowEmail: "chung@kbsmedia.co.kr", enteredShowPhone: "1-323-935-5001", enteredShowName: "Euphoria 2025", enteredShowImage: "show_mbc1", enteredShowLogo: "logo_kbs", enteredShowColor: "color_kbs", enteredShowYear: "2023", enteredShowGenre: "Comedy, Thriller", enteredShowDescription: "Shaun Murphy, a young autistic surgeon who has savant syndrome, relocates from a quiet country life to join the surgical unit at the prestigious San Jose St. Bonaventure Hospital -- a move strongly supported by his mentor, Dr. Aaron Glassman. Having survived a troubled childhood, Shaun is alone in the world and unable to personally connect with those around him, but he finds his niche using his extraordinary medical skill and intuition to save lives and challenge the skepticism of his colleagues.", enteredShowTime: "70 Minutes", enteredShowEpisodes: "17 Episodes", enteredShowUrl: "https://github.com/kwongavin/kformatscreening/blob/main/muscle1.mp4?raw=true")
             .environmentObject(CompanyModel())
     }
 }
